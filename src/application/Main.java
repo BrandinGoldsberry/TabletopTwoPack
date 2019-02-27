d9f7f075251c1c56327b946c3e6e1329b16c9741
KingGopher <47904796+BrandinGoldsberry@users.noreply.github.com>
2019-02-27 16:41:28 -0700

    Moot

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,8 +10,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane noot = new BorderPane();
-			Scene scene = new Scene(noot,400,400);
+			BorderPane moot = new BorderPane();
+			Scene scene = new Scene(moot,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
