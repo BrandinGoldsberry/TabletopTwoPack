04f1d195a47a37081fa55461e30343d32dfdf1d4
YooperScum <47546105+YooperScum@users.noreply.github.com>
2019-02-28 09:52:40 -0700

    Test commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,7 +5,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-
+//Test Commit
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
