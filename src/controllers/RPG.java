ad1dc4a568a69c0f43a9516f4224f250538b852f
ItsRiles <47341306+ItsRiles@users.noreply.github.com>
2019-03-01 00:35:37 -0700

    Stubbed RPG Controller

---
+++ src/controllers/RPG.java
@@ -0,0 +1,51 @@
+package controllers;
+
+import models_RPG.Dungeon;
+import models_RPG.Hero;
+
+public class RPG {
+	private String campaignName;
+	private Hero player;
+	private int playerDungeonLocationX;
+	private int playerDungeomLocationY;
+	private int playerLocationRoomX;
+	private int playerLocationRoomY;
+	private int currentFloorNum;
+	private Dungeon currentFloorDungeon;
+	private int battleTurn;
+	
+	public static void run() {
+		
+	}
+	
+	public static void movePlayer(int playerLocation) {
+		
+	}
+	
+	public static boolean collisionCheck(int playerLocation) {
+		return false;
+		
+	}
+	
+	public static boolean randomEncounter() {
+		return false;
+		
+	}
+	
+	public static void battleProcessing() {
+		
+	}
+	
+	public static void playerTurn() {
+		
+	}
+	
+	public static void enemyTurn() {
+		
+	}
+	
+	public static void battleResults() {
+		
+	}
+		
+}
