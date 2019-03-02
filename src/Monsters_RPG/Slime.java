50cda8228bf303b4d9e9a2ca6dd3398bf7792fd3
Hunter Lemley <hlemley@student.neumont.edu>
2019-03-01 17:58:02 -0700

    Update Slime.java

--- src/Monsters_RPG/Slime.java
+++ src/Monsters_RPG/Slime.java
@@ -7,7 +7,7 @@ public class Slime extends Monster {
 
 	public Slime(String name, int baseHP, int currentHP, int baseMP, int currentMP, int str, int dex, int mag, int luc,
 			int defenseRating, Item heldItem) {
-		super(name, baseHP, currentHP, baseMP, currentMP, str, dex, mag, luc, defenseRating, heldItem);
+		super(name, baseHP, currentHP, baseMP, currentMP, str, heldItem);
 		// TODO Auto-generated constructor stub
 	}
 
