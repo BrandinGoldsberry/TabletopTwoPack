9cd8bfe228cc032d31b9d9b8a6689b89f429a49e
Hunter Lemley <hlemley@student.neumont.edu>
2019-03-03 18:39:43 -0700

    Rat King coded

--- src/Monsters_RPG/RatKing.java
+++ src/Monsters_RPG/RatKing.java
@@ -1,14 +1,24 @@
 package Monsters_RPG;
 
+import items.BottledLightning;
 import models_RPG.Item;
 import models_RPG.Monster;
 
 public class RatKing extends Monster {
 
-	public RatKing(String name, int baseHP, int currentHP, int baseMP, int currentMP, int str, int dex, int mag,
-			int luc, int defenseRating, Item heldItem) {
-		super(name, baseHP, currentHP, baseMP, currentMP, str, heldItem);
-		// TODO Auto-generated constructor stub
+	public RatKing(String name, int str, int dex, int mag, int luc, int defenseRating, Item heldItem) {
+		super(name, str, dex, mag, luc);
+		this.setStr(8);
+		this.setDex(8);
+		this.setMag(8);
+		this.setLuc(8);
+		this.setBaseHP(30 + (3 * str));
+		this.setCurrentHP(this.getBaseHP());
+		this.setBaseMP(20 + (2 * mag));
+		this.setCurrentMP(this.getBaseMP());
+		this.setDefenseRating(2);
+		this.setHeldItem(new BottledLightning("Bottled Lightning"));
+		
 	}
 
 
