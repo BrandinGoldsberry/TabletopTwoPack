df66cffb03d22c9f884c57401b7e8599a12fea68
Hunter Lemley <hlemley@student.neumont.edu>
2019-03-03 18:39:28 -0700

    Giga Slime coded

--- src/Monsters_RPG/GigaSlime.java
+++ src/Monsters_RPG/GigaSlime.java
@@ -1,14 +1,23 @@
 package Monsters_RPG;
 
+import items.MagUp;
 import models_RPG.Item;
 import models_RPG.Monster;
 
 public class GigaSlime extends Monster {
 
-	public GigaSlime(String name, int baseHP, int currentHP, int baseMP, int currentMP, int str, int dex, int mag,
-			int luc, int defenseRating, Item heldItem) {
-		super(name, baseHP, currentHP, baseMP, currentMP, str, heldItem);
-		// TODO Auto-generated constructor stub
+	public GigaSlime(String name, int str, int dex, int mag, int luc, int defenseRating, Item heldItem) {
+		super(name, str, dex, mag, luc);
+		this.setStr(14);
+		this.setDex(14);
+		this.setMag(0);
+		this.setLuc(0);
+		this.setBaseHP(30 + (3 * str));
+		this.setCurrentHP(this.getBaseHP());
+		this.setBaseMP(20 + (2 * mag));
+		this.setCurrentMP(this.getBaseMP());
+		this.setDefenseRating(1);
+		this.setHeldItem(new MagUp("Mag Up"));
 	}
 
 
