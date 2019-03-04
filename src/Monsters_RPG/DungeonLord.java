ec7b61d622e13bae2b5962f559485cb73deb82a9
Hunter Lemley <hlemley@student.neumont.edu>
2019-03-03 18:39:10 -0700

    Dungeon Lord coded

--- src/Monsters_RPG/DungeonLord.java
+++ src/Monsters_RPG/DungeonLord.java
@@ -1,14 +1,23 @@
 package Monsters_RPG;
 
+import items.DexUp;
 import models_RPG.Item;
 import models_RPG.Monster;
 
 public class DungeonLord extends Monster {
 
-	public DungeonLord(String name, int baseHP, int currentHP, int baseMP, int currentMP, int str, int dex, int mag,
-			int luc, int defenseRating, Item heldItem) {
-		super(name, baseHP, currentHP, baseMP, currentMP, str, dex, mag, luc, defenseRating, heldItem);
-		// TODO Auto-generated constructor stub
+	public DungeonLord(String name, int str, int dex, int mag, int luc, int defenseRating, Item heldItem) {
+		super(name, str, dex, mag, luc);
+		this.setStr(15);
+		this.setDex(15);
+		this.setMag(15);
+		this.setLuc(18);
+		this.setBaseHP(30 + (3 * str));
+		this.setCurrentHP(this.getBaseHP());
+		this.setBaseMP(20 + (2 * mag));
+		this.setCurrentMP(this.getBaseMP());
+		this.setDefenseRating(10);
+		this.setHeldItem(null);
 	}
 
 }
