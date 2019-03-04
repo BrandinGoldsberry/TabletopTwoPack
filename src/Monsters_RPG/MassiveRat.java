7d110b22503ee9d09c00348ed84f6b7a029ecb43
Hunter Lemley <hlemley@student.neumont.edu>
2019-03-03 18:39:35 -0700

    Massive Rat coded

--- src/Monsters_RPG/MassiveRat.java
+++ src/Monsters_RPG/MassiveRat.java
@@ -1,14 +1,23 @@
 package Monsters_RPG;
 
+import items.HiPotion;
 import models_RPG.Item;
 import models_RPG.Monster;
 
 public class MassiveRat extends Monster {
 
-	public MassiveRat(String name, int baseHP, int currentHP, int baseMP, int currentMP, int str, int dex, int mag,
-			int luc, int defenseRating, Item heldItem) {
-		super(name, baseHP, currentHP, baseMP, currentMP, str, heldItem);
-		// TODO Auto-generated constructor stub
+	public MassiveRat(String name, int str, int dex, int mag, int luc, int defenseRating, Item heldItem) {
+		super(name, str, dex, mag, luc);
+		this.setStr(8);
+		this.setDex(4);
+		this.setMag(0);
+		this.setLuc(4);
+		this.setBaseHP(30 + (3 * str));
+		this.setCurrentHP(this.getBaseHP());
+		this.setBaseMP(20 + (2 * mag));
+		this.setCurrentMP(this.getBaseMP());
+		this.setDefenseRating(2);
+		this.setHeldItem(new HiPotion("Hi-Potion"));
 	}
 
 
