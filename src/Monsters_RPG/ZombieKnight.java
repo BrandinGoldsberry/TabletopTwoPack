45aecde5f931d8d4dd64931b63385f4701f3b2f6
Hunter Lemley <hlemley@student.neumont.edu>
2019-03-03 18:40:09 -0700

    Zombie Knight coded

--- src/Monsters_RPG/ZombieKnight.java
+++ src/Monsters_RPG/ZombieKnight.java
@@ -1,14 +1,23 @@
 package Monsters_RPG;
 
+import items.Javelin;
 import models_RPG.Item;
 import models_RPG.Monster;
 
 public class ZombieKnight extends Monster {
 
-	public ZombieKnight(String name, int baseHP, int currentHP, int baseMP, int currentMP, int str, int dex, int mag,
-			int luc, int defenseRating, Item heldItem) {
-		super(name, baseHP, currentHP, baseMP, currentMP, str, heldItem);
-		// TODO Auto-generated constructor stub
+	public ZombieKnight(String name, int str, int dex, int mag, int luc, int defenseRating, Item heldItem) {
+		super(name, str, dex, mag, luc);
+		this.setStr(8);
+		this.setDex(9);
+		this.setMag(6);
+		this.setLuc(8);
+		this.setBaseHP(30 + (3 * str));
+		this.setCurrentHP(this.getBaseHP());
+		this.setBaseMP(20 + (2 * mag));
+		this.setCurrentMP(this.getBaseMP());
+		this.setDefenseRating(6);
+		this.setHeldItem(new Javelin("Javelin"));
 	}
 
 
