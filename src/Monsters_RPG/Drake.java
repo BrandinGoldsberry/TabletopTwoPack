1a5b1122c7faa17441fb399bc4ccacf0cc330827
Hunter Lemley <hlemley@student.neumont.edu>
2019-03-03 18:39:03 -0700

    Drake Coded

--- src/Monsters_RPG/Drake.java
+++ src/Monsters_RPG/Drake.java
@@ -1,14 +1,23 @@
 package Monsters_RPG;
 
+import items.StrUp;
 import models_RPG.Item;
 import models_RPG.Monster;
 
 public class Drake extends Monster {
 
-	public Drake(String name, int baseHP, int currentHP, int baseMP, int currentMP, int str, int dex, int mag, int luc,
-			int defenseRating, Item heldItem) {
-		super(name, baseHP, currentHP, baseMP, currentMP, str, dex, mag, luc, defenseRating, heldItem);
-		// TODO Auto-generated constructor stub
+	public Drake(String name, int str, int dex, int mag, int luc, int defenseRating, Item heldItem) {
+		super(name, str, dex, mag, luc);
+		this.setStr(5);
+		this.setDex(11);
+		this.setMag(11);
+		this.setLuc(4);
+		this.setBaseHP(30 + (3 * str));
+		this.setCurrentHP(this.getBaseHP());
+		this.setBaseMP(20 + (2 * mag));
+		this.setCurrentMP(this.getBaseMP());
+		this.setDefenseRating(3);
+		this.setHeldItem(new StrUp("Str Up"));
 	}
 	
 	
