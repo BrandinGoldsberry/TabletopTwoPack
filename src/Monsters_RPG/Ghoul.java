b85baf2b4f8c8e8ee20df8adecdddb7ac61f8eaa
Hunter Lemley <hlemley@student.neumont.edu>
2019-03-03 18:39:16 -0700

    Ghoul coded

--- src/Monsters_RPG/Ghoul.java
+++ src/Monsters_RPG/Ghoul.java
@@ -1,14 +1,23 @@
 package Monsters_RPG;
 
+import items.DexUp;
 import models_RPG.Item;
 import models_RPG.Monster;
 
 public class Ghoul extends Monster {
 
-	public Ghoul(String name, int baseHP, int currentHP, int baseMP, int currentMP, int str, int dex, int mag, int luc,
-			int defenseRating, Item heldItem) {
-		super(name, baseHP, currentHP, baseMP, currentMP, str, dex, mag, luc, defenseRating, heldItem);
-		// TODO Auto-generated constructor stub
+	public Ghoul(String name, int str, int dex, int mag, int luc, int defenseRating, Item heldItem) {
+		super(name, str, dex, mag, luc);
+		this.setStr(4);
+		this.setDex(3);
+		this.setMag(3);
+		this.setLuc(4);
+		this.setBaseHP(30 + (3 * str));
+		this.setCurrentHP(this.getBaseHP());
+		this.setBaseMP(20 + (2 * mag));
+		this.setCurrentMP(this.getBaseMP());
+		this.setDefenseRating(0);
+		this.setHeldItem(new DexUp("Dex Up"));
 	}
 
 
